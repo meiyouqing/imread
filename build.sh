45c02e154cdec02f1aebaf672431c85d74818f5e
MeiMingming <meimingming@jndl.mobi>
2016-09-08 14:04:32 +0800

    before clear up

--- build.sh
+++
@@ -1,5 +0,0 @@
-rm -rf p
-mkdir p
-mkdir p/src
-cp -r src/img p/src/img
-webpack
--- server-p.js
+++ server-p.js
@@ -14,7 +14,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'p/index.html'))
 })
 
-var PORT = process.env.PORT || 8080
+var PORT = process.argv[2] || 8080
 app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT)
 })
