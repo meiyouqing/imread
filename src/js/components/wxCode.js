24e056e50e1e840f9bbe76674037f67aa47a716e
Wu xiangang <wu532930610@qq.com>
2016-12-05 17:04:25 +0800

    j

---
+++ src/js/components/wxCode.js
@@ -0,0 +1,35 @@
+import parseQuery from '../modules/parseQuery'
+import browserHistory from 'react-router/lib/browserHistory'
+import AJAX from '../modules/AJAX'
+import React from 'react'
+import Mixins from '../modules/mixins'
+import storage from '../modules/storage'
+
+var WxCode = React.createClass({
+	mixins: [Mixins()],
+    componentDidMount: function(){
+		var that = this;
+		this.from = parseQuery(location.search);
+
+        if(!this.from.code){
+            POP._alert(this.from.code+' 登录失败');
+            browserHistory.replace('/');
+            return;
+
+        }
+        if(this.isWx() && this.from  && this.from.code && this.from.state == 'wxPay') {
+                if(this.from.callback){
+                    alert(this.from.callback+location.search)
+                    location.href = this.from.callback+location.search;
+                }
+                else
+                    location.href="http://223.95.81.227:8082/pay/wxauth"
+        }
+    },
+    render:function(){
+        return  (<div className="gg-body"> 
+                <p className="WxLogin">正在微信验证...</p>
+            </div>)
+    }
+})
+module.exports = WxCode;
