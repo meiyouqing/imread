5b7fcbe49cc73dda6afaadf83a57f9c792775a0a
MeiMingming <meimingming@jndl.mobi>
2016-12-15 09:59:11 +0800

    product htmlContent with http to https

--- server/htmlContent.js
+++ server/htmlContent.js
@@ -34,6 +34,9 @@ export const renderFullPage = (html, preloadedState) => {
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/src/img/weblogo.png" />
         <script>
+            if(location.protocol === 'http:'){
+              location = location.href.replace('http:','https:')
+            }
             (function () {   
                 var ua = window.navigator.userAgent.toLowerCase(), code;
                 if(/micromessenger/.test(ua)) {
