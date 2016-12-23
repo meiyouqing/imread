dd3e1ccd409a30801b1c092fc3940fcc1ad2aa7a
MeiMingming <meimingming@jndl.mobi>
2016-12-23 14:19:11 +0800

    merge the eslinted

--- server/server-dev.js
+++ server/server-dev.js
@@ -99,7 +99,7 @@ app.get('*', (req, res) => {
   });
 });
 
-const port = process.argv[2] || 80;
+const port = process.argv[2] || 8080;
 app.listen(port, (error) => {
   if (error) {
     console.error(error);
